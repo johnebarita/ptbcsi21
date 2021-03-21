64ff1b953bc8f9f805d2c73ef848466cbc7422b7
Kyle Castillon <kyle.ptbcsi@gmail.com>
2021-03-21 15:17:42 +0800

    added profile edit ui

--- app/Views/auth/register.blade.php
+++ app/Views/auth/register.blade.php
@@ -1,35 +1,45 @@
 @extends('layouts.guest')
 @section('main')
 
-<h1>{{ lang('Auth.registration') }}</h1>
+	<!-- Sign up form -->
+	<section class="vh-100 flex">
+		<div class="container my-auto">
+			@include('auth._notifications')
+			<div class="signup-content">
+				<div class="signup-form">
+					<h2 class="form-title">{{ lang('Auth.registration') }}</h2>
+					<form method="POST" action="{{ route_to('register') }}" accept-charset="UTF-8"
+						  onsubmit="registerButton.disabled = true; return true;" class="register-form" id="register-form">
 
-@include('auth._notifications')
+						@csrf
 
-<form method="POST" action="{{ route_to('register') }}" accept-charset="UTF-8"
-	onsubmit="registerButton.disabled = true; return true;">
-	@csrf
-	<p>
-	    <label>{{ lang('Auth.name') }}</label><br />
-	    <input required minlength="2" type="text" name="name" value="{{ old('name') }}" />
-	</p>
-	<p>
-	    <label>{{ lang('Auth.email') }}</label><br />
-	    <input required type="email" name="email" value="{{ old('email') }}" />
-	</p>
-	<p>
-	    <label>{{ lang('Auth.password') }}</label><br />
-	    <input required minlength="5" type="password" name="password" value="" />
-	</p>
-	<p>
-	    <label>{{ lang('Auth.passwordAgain') }}</label><br />
-	    <input required minlength="5" type="password" name="password_confirm" value="" />
-	</p>
-	<p>
-	    <button name="registerButton" type="submit">{{ lang('Auth.register') }}</button>
-	</p>
-	<p>
-		<a href="{{ route_to('login') }}" class="float-right">{{ lang('Auth.alreadyRegistered') }}</a>
-	</p>
-</form>
+						<div class="form-group">
+							<label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+							<input required minlength="2" type="text" name="name" value="{{ old('name') }}" id="name" placeholder="Your Name"/>
+						</div>
+						<div class="form-group">
+							<label for="email"><i class="zmdi zmdi-email"></i></label>
+							<input required type="email" name="email" value="{{ old('email') }}" id="email" placeholder="Your Email"/>
+						</div>
+						<div class="form-group">
+							<label for="pass"><i class="zmdi zmdi-lock"></i></label>
+							<input required minlength="5" type="password" name="password" id="pass" placeholder="Password"/>
+						</div>
+						<div class="form-group">
+							<label for="re_pass"><i class="zmdi zmdi-lock-outline"></i></label>
+							<input required minlength="5" type="password" name="password_confirm" id="re_pass" placeholder="Repeat your password"/>
+						</div>
+						<div class="form-group form-button">
+							<input type="submit" name="signup" id="signup" class="form-submit" value="{{ lang('Auth.register') }}"/>
+						</div>
+					</form>
+				</div>
+				<div class="signup-image">
+					<figure><img src="assets/images/signup-image.jpg" alt="sing up image"></figure>
+					<a href="{{ route_to('login') }}" class="signup-image-link">I am already member</a>
+				</div>
+			</div>
+		</div>
+	</section>
 
 @endsection
