54e3bb7f87485a4e9aa71fd6e7a3ead6bd8943d1
John Ebarita <ebarita00@gmail.com>
2021-04-09 19:13:26 +0800

    updated baseURL dependent on env

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://ptbcsi21.test';
+	public $baseURL ='';
 
 	/**
 	 * --------------------------------------------------------------------------
