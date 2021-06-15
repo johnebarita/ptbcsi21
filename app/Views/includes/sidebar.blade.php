c77e3fd24db7df5c4e32b87919472d9fabe55e41
John Ebarita <ebarita00@gmail.com>
2021-06-15 22:20:38 +0800

    added authentication filters and implemented login

--- app/Views/includes/sidebar.blade.php
+++ app/Views/includes/sidebar.blade.php
@@ -9,17 +9,19 @@
         <div class="sidebar-brand-text mx-3">PTBCSI</div>
     </a>
 
-@if(session()->userData['role']=='Admin'||session()->userData['role']=='Payroll Master')
-    <!-- Divider -->
-        <hr class="sidebar-divider my-0">
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item {{$active=="dashboard"?'active':''}}">
-            <a class="nav-link" href="{{route_to('dashboard.index')}}">
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
-        </li>
-                <!-- Divider -->
-        <hr class="sidebar-divider">
+@if(isset(session()->userData))
+    @if(session()->userData['role']=='Admin'||session()->userData['role']=='Payroll Master')
+        <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item {{$active=="dashboard"?'active':''}}">
+                <a class="nav-link" href="{{route_to('dashboard.index')}}">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+    @endif
 @endif
 <!-- Heading -->
     <div class="sidebar-heading">
@@ -51,14 +53,16 @@
             </div>
         </div>
     </li>
-@if(session()->userData['role']=='Admin')
-    <!-- Nav Item - Schedule -->
-        <li class="nav-item {{$active=="schedule"?'active':''}}">
-            <a class="nav-link" href="{{route_to("schedule.index")}}">
-                <i class="fas fa-clock"></i>
-                <span>Schedule</span></a>
-        </li>
+@if(isset(session()->userData))
+    @if(session()->userData['role']=='Admin')
+        <!-- Nav Item - Schedule -->
+            <li class="nav-item {{$active=="schedule"?'active':''}}">
+                <a class="nav-link" href="{{route_to("schedule.index")}}">
+                    <i class="fas fa-clock"></i>
+                    <span>Schedule</span></a>
+            </li>
 
+    @endif
 @endif
 <!-- Nav Item - Calendar -->
     <li class="nav-item {{$active=="calendar"?'active':''}}">
@@ -66,89 +70,93 @@
             <i class="fas fa-calendar-alt"></i>
             <span>Calendar</span></a>
     </li>
-@if(session()->userData['role']=='Admin'||session()->userData['role']=='Payroll Master')
-    <!-- Nav Item - Employees Collapse Menu -->
-        @if(session()->userData['role']=='Admin')
-            <li class="nav-item {{$active=="employee"?'active':($active=="position"?'active':'')}}">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#employeeCollapse"
-                   aria-expanded="true" aria-controls="employeeCollapse">
-                    <i class="fas fa-users"></i>
-                    <span>Employees</span>
-                </a>
-                <div id="employeeCollapse" class="on-top collapse
+@if(isset(session()->userData))
+    @if(session()->userData['role']=='Admin'||session()->userData['role']=='Payroll Master')
+        <!-- Nav Item - Employees Collapse Menu -->
+
+            @if(session()->userData['role']=='Admin')
+                <li class="nav-item {{$active=="employee"?'active':($active=="position"?'active':'')}}">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#employeeCollapse"
+                       aria-expanded="true" aria-controls="employeeCollapse">
+                        <i class="fas fa-users"></i>
+                        <span>Employees</span>
+                    </a>
+                    <div id="employeeCollapse" class="on-top collapse
 {{--{{$active=="employee"?'show':($active=="position"?'show':'')}}--}}
-                        "
-                     aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                            "
+                         aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Employee Management</h6>
+                            <a class="collapse-item {{$active=="employee"?'active':''}}"
+                               href="{{route_to('employee.index')}}">Employee
+                                List</a>
+                            <a class="collapse-item {{$active=="position"?'active':''}}"
+                               href="{{route_to('position.index')}}">Position</a>
+                        </div>
+                    </div>
+                </li>
+            @endif
+
+        <!-- Nav Item - Payroll -->
+            <li class="nav-item {{$active=="payroll"?'active':''}}">
+                <a class="nav-link" href="{{route_to('payroll.index')}}">
+                    <i class="fas fa-money-bill"></i>
+                    <span>Payroll</span></a>
+            </li>
+
+            <!-- Nav Item - Cash Advance -->
+            <li class="nav-item {{$active=="cash-advance"?'active':''}}">
+                <a class="nav-link" href="{{route_to('cash-advance.index')}}">
+                    <i class="fas fa-cash-register"></i>
+                    <span>Cash Advance</span></a>
+            </li>
+
+            <!-- Nav Item - Deductions -->
+            <li class="nav-item {{$active=="sss-contribution-table"?'active':
+                        ($active=="late-penalty"?'active':
+                        (($active=="pag-ibig-contribution-table"?'active':'')))}}">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#deductionCollapse"
+                   aria-expanded="true"
+                   aria-controls="deductionCollapse">
+                    <i class="fas fa-chart-line"></i>
+                    <span>Deduction</span>
+                </a>
+                <div id="deductionCollapse" class="on-top collapse" aria-labelledby="headingTwo"
+                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Employee Management</h6>
-                        <a class="collapse-item {{$active=="employee"?'active':''}}"
-                           href="{{route_to('employee.index')}}">Employee
-                            List</a>
-                        <a class="collapse-item {{$active=="position"?'active':''}}"
-                           href="{{route_to('position.index')}}">Position</a>
+                        <h6 class="collapse-header">Deduction Management:</h6>
+                        <a class="collapse-item {{$active=="sss-contribution-table"?'active':''}}"
+                           href="{{route_to('sss-contribution-table.index')}}">SSS Contribution</a>
+                        <a class="collapse-item {{$active=="pag-ibig-contribution-table"?'active':''}} "
+                           href="{{route_to('pag-ibig-contribution-table.index')}}">Pag-ibig Contribution </a>
+                        <a class="collapse-item {{$active=="phil-health-contribution-table"?'active':''}} "
+                           href="{{route_to('phil-health-contribution-table.index')}}">Philhealth Contribution</a>
+                        {{--                <a class="collapse-item {{$active=="late-penalty"?'active':''}} " href="{{route_to('late-penalty.index')}}">Late Penalty</a>--}}
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Reports Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#reportCollapse"
+                   aria-expanded="true"
+                   aria-controls="reportCollapse">
+                    <i class="fas fa-chart-line"></i>
+                    <span>Reports</span>
+                </a>
+                <div id="reportCollapse" class="on-top collapse" aria-labelledby="headingTwo"
+                     data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Summary Report:</h6>
+                        @if(session()->userData['role']=='Admin')
+                            <a class="collapse-item" href="{{route_to('attendance-report.index')}}">Attendance
+                                Report</a>
+                        @endif
+                        <a class="collapse-item" href="{{route_to('payroll-report.index')}}">Payroll Report</a>
                     </div>
                 </div>
             </li>
         @endif
-
-    <!-- Nav Item - Payroll -->
-        <li class="nav-item {{$active=="payroll"?'active':''}}">
-            <a class="nav-link" href="{{route_to('payroll.index')}}">
-                <i class="fas fa-money-bill"></i>
-                <span>Payroll</span></a>
-        </li>
-
-        <!-- Nav Item - Cash Advance -->
-        <li class="nav-item {{$active=="cash-advance"?'active':''}}">
-            <a class="nav-link" href="{{route_to('cash-advance.index')}}">
-                <i class="fas fa-cash-register"></i>
-                <span>Cash Advance</span></a>
-        </li>
-
-        <!-- Nav Item - Deductions -->
-        <li class="nav-item {{$active=="sss-contribution-table"?'active':
-                        ($active=="late-penalty"?'active':
-                        (($active=="pag-ibig-contribution-table"?'active':'')))}}">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#deductionCollapse"
-               aria-expanded="true"
-               aria-controls="deductionCollapse">
-                <i class="fas fa-chart-line"></i>
-                <span>Deduction</span>
-            </a>
-            <div id="deductionCollapse" class="on-top collapse" aria-labelledby="headingTwo"
-                 data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Deduction Management:</h6>
-                    <a class="collapse-item {{$active=="sss-contribution-table"?'active':''}}"
-                       href="{{route_to('sss-contribution-table.index')}}">SSS Contribution</a>
-                    <a class="collapse-item {{$active=="pag-ibig-contribution-table"?'active':''}} "
-                       href="{{route_to('pag-ibig-contribution-table.index')}}">Pag-ibig Contribution </a>
-                    <a class="collapse-item {{$active=="phil-health-contribution-table"?'active':''}} "
-                       href="{{route_to('phil-health-contribution-table.index')}}">Philhealth Contribution</a>
-                    {{--                <a class="collapse-item {{$active=="late-penalty"?'active':''}} " href="{{route_to('late-penalty.index')}}">Late Penalty</a>--}}
-                </div>
-            </div>
-        </li>
-
-        <!-- Nav Item - Reports Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#reportCollapse"
-               aria-expanded="true"
-               aria-controls="reportCollapse">
-                <i class="fas fa-chart-line"></i>
-                <span>Reports</span>
-            </a>
-            <div id="reportCollapse" class="on-top collapse" aria-labelledby="headingTwo"
-                 data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Summary Report:</h6>
-                    @if(session()->userData['role']=='Admin')
-                        <a class="collapse-item" href="{{route_to('attendance-report.index')}}">Attendance Report</a>
-                    @endif
-                    <a class="collapse-item" href="{{route_to('payroll-report.index')}}">Payroll Report</a>
-                </div>
-            </div>
-        </li>
     @endif
     <li class="nav-item {{$active=="bug-report"?'active':''}}">
         <a class="nav-link" href="{{route_to('bug-report.index')}}">
